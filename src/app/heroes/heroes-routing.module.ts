07283c80a8a9b4c38d8994710cade0edb66961e6
imran <azerimrane@gmail.com>
2024-03-31 15:53:55 +0200

    fin de la primera seccion, toda la estructura de componentes y rutas terminada

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,11 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutPageComponent
+    component: LayoutPageComponent,
+    children:[
+      {
+        path: 'newhero',
+        component: NewPageComponent
+      },
+      {
+        path: 'search',
+        component: SearchPageComponent
+      },
+      {
+        path: 'edit/:id',
+        component: NewPageComponent
+      },
+      {
+        path: 'list',
+        component: ListPageComponent
+      },
+      {
+        path: ':id',
+        component: HeroPageComponent
+      }
+      ,
+      {
+        path: '**',
+        redirectTo: 'list'
+      }
+      // ES IMPORTANTE EL ORDEN, EN ESTE CASO EL :ID TIENE QUE IR
+      //EL ULTIMO PORQUE ASI PRIMERO SE COMPRUEBA SI NO PASA POR OTRA RUTA,
+      //SI ESTUVIESE :ID PRIMERO POR EJEMPLO BUSCARIA SEARCH COMO SI FUERA UN ID
+    ]
   }
 ];
 
