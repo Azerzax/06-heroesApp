e2226e7d7bd428cd6b13493182b786c6aea5677d
imran <azerimrane@gmail.com>
2024-04-01 03:01:53 +0200

    estilo lista de heroes

---
+++ src/app/heroes/components/hero-card/hero-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+
+@Component({
+  selector: 'heroes-hero-card',
+  templateUrl: './hero-card.component.html',
+  styleUrl: './hero-card.component.css'
+})
+export class HeroCardComponent {
+
+  @Input()
+  public hero!: Hero;
+
+  ngOnInit(): void {
+    if (!this.hero) {
+      throw Error('Hero es requerido.');
+    }
+  }
+
+}
